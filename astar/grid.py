a5bd190edb2a9ea8786b53815adcdaaf1e96e9bf
Den4200 <dpham.42@hotmail.com>
2020-05-19 21:11:16 -0400

    added support for box removal on grid

--- astar/grid.py
+++ astar/grid.py
@@ -12,6 +12,8 @@ class Grid(InteractiveWidget):
         self.boxes = arcade.ShapeElementList()
         self.filled = arcade.ShapeElementList()
 
+        self.hash_map = dict()
+
         for y in range(0, s.WINDOW_SIZE[1], Tile.SCALED):
             for x in range(0, s.WINDOW_SIZE[0], Tile.SCALED):
                 center_x, center_y = find_grid_box(x, y)
@@ -26,27 +28,49 @@ class Grid(InteractiveWidget):
                     )
                 )
 
-        self.is_pressed = False
+        self.active = True
+        self.should_place = False
+        self.should_break = False
 
     def draw(self) -> None:
-        self.filled.draw()
+        if len(self.filled) > 0:
+            self.filled.draw()
+
         self.boxes.draw()
 
     def move(self, delta_x: float, delta_y: float) -> None:
         pass
 
     def on_mouse_press(self, x: float, y: float, button: int, modifiers: int) -> None:
-        self.is_pressed = not self.is_pressed
+        if self.active:
+            if button == arcade.MOUSE_BUTTON_LEFT:
+                self.should_place = not self.should_place
+                self.should_break = False
+
+            elif button == arcade.MOUSE_BUTTON_RIGHT:
+                self.should_break = not self.should_break
+                self.should_place = False
 
     def on_mouse_motion(self, x: float, y: float, dx: float, dy: float) -> None:
-        if self.is_pressed:
+        if self.active:
             center_x, center_y = find_grid_box(x, y)
-            self.filled.append(
-                arcade.create_rectangle_filled(
-                    center_x=center_x,
-                    center_y=center_y,
-                    width=Tile.SCALED,
-                    height=Tile.SCALED,
-                    color=arcade.color.RED
-                )
-            )
+            box = self.hash_map.get((center_x, center_y))
+
+            if self.should_place:
+                if box is None:
+                    box = arcade.create_rectangle_filled(
+                        center_x=center_x,
+                        center_y=center_y,
+                        width=Tile.SCALED,
+                        height=Tile.SCALED,
+                        color=arcade.color.RED
+                    )
+
+                    self.filled.append(box)
+                    self.hash_map[(center_x, center_y)] = box
+
+            elif self.should_break:
+                if box is not None:
+                    print(center_x, center_y)
+                    self.filled.remove(box)
+                    self.hash_map.pop((center_x, center_y))
