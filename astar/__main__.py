f7108e1293199a3781694e28df16eb3ddf900032
Den4200 <dpham.42@hotmail.com>
2020-05-20 01:31:20 -0400

    create loading screen

--- astar/__main__.py
+++ astar/__main__.py
@@ -1,43 +1,13 @@
 import arcade
-from arcade_ui import EventMap, View
+from arcade_ui import EventMap
 
 from astar import Settings as s
-from astar.grid import Grid
-
-
-class AStarVisualizer(View):
-
-    def __init__(self, event_map: EventMap) -> None:
-        super().__init__(event_map)
-        self.grid = None
-        self.grid_setup = None
-
-        arcade.set_background_color(arcade.color.WHITE)
-
-    def on_show(self) -> None:
-        self.event_map.clear()
-        self.grid = Grid()
-        self.grid_setup = self.grid.setup()
-
-        super().setup()
-
-    def on_draw(self) -> None:
-        arcade.start_render()
-        self.grid.draw()
-
-    def on_update(self, delta_time: float) -> None:
-        if self.grid_setup is not None:
-            try:
-                next(self.grid_setup)
-            except StopIteration:
-                self.grid_setup = None
-
-        super().on_update(delta_time)
+from astar.views import LoadingView
 
 
 def main() -> None:
-    window = arcade.Window(*s.WINDOW_SIZE, 'A* Pathfinding Visualizer')
-    window.show_view(AStarVisualizer(EventMap()))
+    window = arcade.Window(*s.WINDOW_SIZE)
+    window.show_view(LoadingView(EventMap()))
     arcade.run()
 
 
---
+++ astar/views.py
@@ -0,0 +1,80 @@
+import arcade
+from arcade_ui import EventMap, TextBox, View
+
+from astar import Settings as s
+from astar.grid import Grid
+
+
+class AStarVisualization(View):
+
+    def __init__(self, event_map: EventMap, grid: Grid) -> None:
+        super().__init__(event_map)
+        self.grid = grid
+
+        arcade.set_background_color(arcade.color.WHITE)
+
+    def on_show(self) -> None:
+        self.event_map.clear()
+        self.event_map.add(self.grid)
+        super().setup()
+
+    def on_draw(self) -> None:
+        arcade.start_render()
+        self.grid.draw()
+
+
+class LoadingView(View):
+
+    def __init__(self, event_map: EventMap) -> None:
+        super().__init__(event_map)
+
+        self.title = None
+        self.loading_text = None
+
+        self.grid = None
+        self.grid_setup = None
+
+        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
+
+    def on_show(self) -> None:
+        self.event_map.clear()
+
+        self.title = TextBox(
+            text='A* Pathfinding Visualization',
+            center_x=s.WINDOW_SIZE[0] / 2,
+            center_y=s.WINDOW_SIZE[1] / 2 + 50,
+            width=400,
+            height=60
+        )
+
+        self.loading_text = TextBox(
+            text='Loading..',
+            center_x=s.WINDOW_SIZE[0] / 2,
+            center_y=s.WINDOW_SIZE[1] / 2 - 50,
+            width=100,
+            height=30
+        )
+
+        self.grid = Grid()
+        self.grid.active = False
+        self.grid_setup = self.grid.setup()
+
+        super().setup()
+
+    def on_draw(self) -> None:
+        arcade.start_render()
+        self.title.draw()
+        self.loading_text.draw()
+
+    def on_update(self, delta_time: float) -> None:
+        if self.grid_setup is not None:
+            try:
+                next(self.grid_setup)
+            except StopIteration:
+                self.grid_setup = None
+                self.grid.active = True
+                self.window.show_view(
+                    AStarVisualization(self.event_map, self.grid)
+                )
+
+        super().on_update(delta_time)
